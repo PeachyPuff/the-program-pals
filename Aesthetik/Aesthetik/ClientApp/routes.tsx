f32701565dfd8698494fa91fa18b906b3965e9b2
Elizabeth Nelson <lizinlot@gmail.com>
2018-02-13 16:14:30 -0600

    Removed unnecessary files, small Layout changes
    Removed ServerMath.js, NodeResult.cshtml, and all references to them

--- Aesthetik/Aesthetik/ClientApp/routes.tsx
+++ Aesthetik/Aesthetik/ClientApp/routes.tsx
@@ -4,9 +4,11 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { TextArea } from './components/TextArea';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
+    <Route path='/fetchdata' component={FetchData} />
+    <Route path='/textarea' component={TextArea} />
 </Layout>;
--- Aesthetik/Aesthetik/Controllers/SampleDataController.cs
+++ Aesthetik/Aesthetik/Controllers/SampleDataController.cs
@@ -16,7 +16,7 @@ namespace Aesthetik.Controllers
 
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts()
-        {
+        { 
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
