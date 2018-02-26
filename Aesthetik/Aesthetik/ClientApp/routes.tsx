8a24461c9f11467b8868cac3a32518b91618c59b
Elizabeth Nelson <lizinlot@gmail.com>
2018-02-26 14:56:41 -0600

    Changes to Draft Editor, Made route.ts compatible with Javascript files

--- Aesthetik/Aesthetik/ClientApp/routes.tsx
+++ Aesthetik/Aesthetik/ClientApp/routes.tsx
@@ -1,14 +1,17 @@
-import * as React from 'react';
+﻿import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { TextArea } from './components/TextArea';
+import { MyEditor } from './components/MyEditor';
 
 export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
+    <Route exact path='/' component={Home} />
+    <Route path='/counter' component={Counter} />
     <Route path='/fetchdata' component={FetchData} />
     <Route path='/textarea' component={TextArea} />
+
+
 </Layout>;
