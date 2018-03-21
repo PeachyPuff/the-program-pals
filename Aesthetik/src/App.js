3e716946e14189d3724ac88119ad87202fa6bb32
Elizabeth Nelson <lizinlot@gmail.com>
2018-03-20 19:35:02 -0500

    Implemented draft.js editor and applied minimal styling

--- Aesthetik/src/App.js
+++ Aesthetik/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
 import logo from './AesthetikLogo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../node_modules/react-bootstrap';
-
 import { Navbar,
          Nav,
          NavItem,
@@ -11,39 +12,43 @@ import { Navbar,
          MenuItem
         } from '../node_modules/react-bootstrap';
 require:{ '../node_modules/jquery'};
-//require:{'../node_modules/bootstrap'};
+require:{'../node_modules/bootstrap'};
 
-class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
   render() {
     return (
-      <div className="container-fluid">
+         <div className="container-fluid">
       <Navbar>
           <Navbar.Brand>
             <a href="#brand">Aesthetik</a>
         </Navbar.Brand>
-    <Nav>
-      <NavItem eventKey={1} href="#">
-        About
-      </NavItem>
-      <NavItem eventKey={2} href="#">
-        Features
-      </NavItem>
-   
-    </Nav>
-
-    </Navbar>;
-        <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">AESTHETIK</h1>
-        </header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-intro">
-          An atmospheric writing application for your creative needs
-        </p>
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            About
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Features
+          </NavItem>
+        </Nav>
+      </Navbar>;
+    
+      <div id= "content">
+        <h1>Aesthetik</h1>
+        <div className= "editor">
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+          />
+        </div>
       </div>
-   </div>
+      </div>
     );
   }
 }
 
-export default App;
+//export default App;
