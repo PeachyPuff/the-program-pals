8494ca769027654ac6b5ce6a65efe92c0e8d10c8
Elizabeth Nelson <lizinlot@gmail.com>
2018-03-30 20:09:43 -0500

    Reinstalled nodes_modules
    Reinstalled nodes_modules to resolve "multiple instances of React installed" error; uninstalled inline toolbar

--- Aesthetik/src/Editor.js
+++ Aesthetik/src/Editor.js
@@ -2,15 +2,8 @@ import React, { Component } from 'react';
 import logo from './AesthetikLogo.svg';
 import ReactDOM from 'react-dom';
 import {Editor, EditorState} from 'draft-js';
-import './App.css';
-import { Navbar,
-         Nav,
-         NavItem,
-         Button,
-         NavDropdown,
-         MenuItem
-        } from '../node_modules/react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
+
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,20 +13,7 @@ export default class App extends React.Component {
   render() {
     return (
          <div className="container-fluid">
-      <Navbar>
-          <Navbar.Brand>
-            <a href="#brand">Aesthetik</a>
-        </Navbar.Brand>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            About
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Features
-          </NavItem>
-        </Nav>
-      </Navbar>;
-    
+      
       <div id= "content">
         <h1>Aesthetik</h1>
         <div className= "editor">
--- Aesthetik/src/node_modules/draft-js-buttons/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-
-# Change Log
-
-All notable changes to this project will be documented in this file.
-This project adheres to [Semantic Versioning](http://semver.org/).
-
-## 2.0.0-rc9 - 2017-11-07
-- Added buttons for Superscript and Subscript.
-
