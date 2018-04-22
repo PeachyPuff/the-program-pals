95e9e3cc4f8d8f5f7634d738dc8795b1659fcec6
Elizabeth Nelson <lizinlot@gmail.com>
2018-04-22 14:16:32 -0500

    Added MenuBar functionality
    for background customization

--- Aesthetik/src/Editor.js
+++ Aesthetik/src/Editor.js
@@ -14,7 +14,6 @@ import { Modal, Button} from 'react-bootstrap';
 import { StyleSheet, css } from 'aphrodite';
 import Background from './happyBackground1.svg';
 import MenuBar from './MenuBar';
-import Header from './Header';
 
 const sideToolbarPlugin = createSideToolbarPlugin({
   structure: [
@@ -28,7 +27,6 @@ const sideToolbarPlugin = createSideToolbarPlugin({
 const { SideToolbar } = sideToolbarPlugin;
 const plugins = [sideToolbarPlugin];
 
-//onClick={()=>this.setBackground(Background)}>Theme
 
 // componentDidMount () {
 //         const script = document.createElement("script");
@@ -45,62 +43,16 @@ export default class EditorPage extends React.Component {
     super(props);
     this.state = {
       hasFocus: false,
-      show: false,
       editorState: EditorState.createEmpty()};
     this.onChange = (editorState) => this.setState({editorState});
-    this.setBackground = this.setBackground.bind(this);
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
   }
 
-  setBackground(background){
-    document.body.style.backgroundImage= "url("+ background + ")";
-    document.body.style.backgroundSize= "cover";
-  }
-
-  handleClose() {
-    this.setState({ show: false });
-  }
-
-  handleShow() {
-    this.setState({ show: true });
-    console.log("pressed");
-  }
-
-
-  
 
   render() {
     return (
       <div>
-      
+      <MenuBar />
        
-         <ButtonGroup >
-          <Button className='MainButton' onClick={this.handleShow}>Theme </Button>
-          <Button className='MainButton'>Background</Button>
-          <Button className='MainButton'>Tik</Button>
-        </ButtonGroup>;
-
-         <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Choose Your Aesthetik</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h2>e n e r g e t i c.</h2>
-            <Button>Prismatic Sunflower</Button>
-            <Button>Clear Blue</Button> 
-            <hr />
-            <h2>p e a c e f u l. </h2>
-            <Button>Peaceful</Button>
-             <hr />
-            <h2>a u s t e r e. </h2>
-            <Button>Austere</Button> 
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleClose}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-
       <div className={`container-root ${this.state.hasFocus ? 'hasFocus' : ''}`}>
       
       <div id= "content">
--- Aesthetik/src/MenuBar.js
+++ Aesthetik/src/MenuBar.js
@@ -1,31 +1,93 @@
 import React from 'react';
 import { ButtonGroup } from 'react-bootstrap';
 import {BoldButton, ItalicButton, UnderlineButton, CodeButton, HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton,} from 'draft-js-buttons';
-import { Navbar,
-         Nav,
-         NavItem,
+import { Dropdown,
+		 Modal,
          Button,
-         NavDropdown,
          MenuItem,
          Glyphicon,
          Image
         } from 'react-bootstrap';
 import { IndexLinkContainer } from 'react-router-bootstrap/lib';
 import Logo from 'AesthetikLogo.svg';
-
+import Background from './happyBackground1.svg';
 
 export default class MenuBar extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      }
+    this.setBackground = this.setBackground.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+  setBackground(background){
+    document.body.style.backgroundImage= "url("+ background + ")";
+    document.body.style.backgroundSize= "cover";
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+    console.log("pressed");
+  }
 	render(){
 		return(
-			<div>
+		  <div>
+      	 	<Button onClick={this.handleShow}>
+      		  <Glyphicon glyph="edit" /> 
+      		</Button>
+
+              <Dropdown>
+      		   <Dropdown.Toggle>
+      			<Glyphicon glyph="music" /> 
+      		   </Dropdown.Toggle>
+      		   <Dropdown.Menu className="super-colors">
+      			 <MenuItem eventKey="1">Times New Roman</MenuItem>
+      			 <MenuItem eventKey="2">Georgia</MenuItem>
+      			 <MenuItem eventKey="1">Times New Roman</MenuItem>
+      			 <MenuItem eventKey="2">Georgia</MenuItem>
+    			</Dropdown.Menu>
+    		</Dropdown>
+    	
+
+      		 <Dropdown>
+      		   <Dropdown.Toggle>
+      			<Glyphicon glyph="font" /> 
+      		   </Dropdown.Toggle>
+      		   <Dropdown.Menu className="super-colors">
+      			 <MenuItem eventKey="1">Times New Roman</MenuItem>
+      			 <MenuItem eventKey="2">Georgia</MenuItem>
+      			 <MenuItem eventKey="1">Times New Roman</MenuItem>
+      			 <MenuItem eventKey="2">Georgia</MenuItem>
+    			</Dropdown.Menu>
+    			</Dropdown>
 			
-            			<Button>Change Theme</Button>
-      					<Button>Change Tik</Button>
-      					<Button>Font</Button>
-      					<Button>Save</Button>
-    		
-		
-			</div>
+			<Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Choose Your Aesthetik</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h2>e n e r g e t i c.</h2>
+            <Button onClick={()=>this.setBackground(Background)}>Prismatic Sunflower</Button>
+            <Button>Clear Blue</Button> 
+            <hr />
+            <h2>p e a c e f u l. </h2>
+            <Button>Peaceful</Button>
+             <hr />
+            <h2>a u s t e r e. </h2>
+            <Button>Austere</Button> 
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+        </div>
+
 			)
 	}
 }
